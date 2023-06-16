49746301a9ef28c1356a64cf5d11eba367c8fdca
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2023-06-16 16:38:08 -0300

    feat: readme and changes on my position

--- src/data/homeInfosData.ts
+++ src/data/homeInfosData.ts
@@ -1,6 +1,6 @@
 const homeInfosData = {
-    position: "DESENVOLVEDOR FULL STACK",
-    position2: "Desenvolvedor Full Stack",
+    position: "DESENVOLVEDOR FRONT END",
+    position2: "Desenvolvedor Front End",
     description:
         "Seja bem-vindo(a) ao meu site pessoal. Aqui, além de encontrar todas as informações que precisa sobre mim, você poderá visualizar na prática como funciona meu método de trabalho.",
 };
