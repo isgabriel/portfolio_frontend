28d908a475e004fb1e8a057e1b4fd10dd7c821b1
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-03-25 18:26:28 -0300

    feat: new changes on contact modal

--- src/components/ModalContact/index.tsx
+++ src/components/ModalContact/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import closeBtn from "../../assets/general-icons/close-btn/close-btn-mobile.svg";
 
 import { useContext, useState } from "react";
@@ -31,14 +32,20 @@ const ModalContact = () => {
             message: message,
         };
 
-        handleCloseModal();
         emailjs
             .send(serviceId, templateId, templateParams, publicKey)
             .then((response) => {
-                console.log("Email enviado com sucesso", response);
                 setName("");
                 setEmail("");
                 setMessage("");
+
+                const botao = document.getElementById("sendBtn");
+                botao!.innerText = "Email Enviado!";
+
+                setTimeout(() => {
+                    // alert(`Email enviado com sucesso, ${name}`);
+                    handleCloseModal();
+                }, 2000);
             })
             .catch((error) => {
                 console.log("Erro ao enviar email", error);
@@ -46,51 +53,55 @@ const ModalContact = () => {
     };
 
     return (
-        <StyledModalBox>
-            <SectionModal>
-                <div>
+        <>
+            <StyledModalBox>
+                <SectionModal>
                     <div>
-                        <button onClick={handleCloseModal}>
-                            <img src={closeBtn} alt="" />
-                        </button>
+                        <div>
+                            <button onClick={handleCloseModal}>
+                                <img src={closeBtn} alt="" />
+                            </button>
+                        </div>
+                        <h2>FORMULÁRIO DE CONTATO</h2>
+                        <StyledForm onSubmit={sendEmail} id="contat-form">
+                            <div>
+                                <input
+                                    type="text"
+                                    placeholder="Nome"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <input
+                                    type="email"
+                                    placeholder="Email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <textarea
+                                    name="message"
+                                    id=""
+                                    placeholder="Digite sua mensagem"
+                                    value={message}
+                                    onChange={(e) => setMessage(e.target.value)}
+                                    required
+                                ></textarea>
+                            </div>
+                            <section>
+                                <button id="sendBtn" type="submit">
+                                    Enviar
+                                </button>
+                            </section>
+                        </StyledForm>
                     </div>
-                    <h2>FORMULÁRIO DE CONTATO</h2>
-                    <StyledForm onSubmit={sendEmail} id="contat-form">
-                        <div>
-                            <input
-                                type="text"
-                                placeholder="Nome"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <input
-                                type="email"
-                                placeholder="Email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <textarea
-                                name="message"
-                                id=""
-                                placeholder="Digite sua mensagem"
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                                required
-                            ></textarea>
-                        </div>
-                        <section>
-                            <button type="submit">Enviar</button>
-                        </section>
-                    </StyledForm>
-                </div>
-            </SectionModal>
-        </StyledModalBox>
+                </SectionModal>
+            </StyledModalBox>
+        </>
     );
 };
 
